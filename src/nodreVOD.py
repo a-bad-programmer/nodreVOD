b1a2569fe7a8a80a5e5e4e9daae0d4a335ffaec8
a-bad-programmer <quintinf19@parkschool.net>
2020-02-06 14:35:05 -0500

    Rework, re-wrote keyboard, mouse functions. disabled random drag

--- src/nodreVOD.py
+++ src/nodreVOD.py
@@ -1,81 +1,26 @@
-import keyboard, mouse
-import pyperclip, time
-import random, numpy, threading
-import sys
-
+import random, numpy, keyboard, mouse, time, pyperclip, threading
 layout = [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'],
           ['tab','q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
           ['capslock','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'enter'],
           ['left shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'right shift'],
           ['left ctrl', 'windows', 'alt', 'space','space','space','space','space','right alt','menu','right ctrl']
           ]
-amt = 5
-global lock
-lock = threading.Semaphore(1)
-schlock = threading.Semaphore(1)
-global excludes
-excludes = []
+
+keyboardOdds = .5
+missClickChance = 10
+clipClearCooldown = 5*60
+randomDragChance = 50
 has_moved = False
-#keyboard.remap_key('a', 'right shift')
+
+
+
+
 def randkey():
-    while True:
-        column = numpy.random.randint(len(layout))
-        # print(layout[column])
-        cell = numpy.random.randint(len(layout[column]))
-        # print(cell)
-        key = layout[column][cell]
-        if key not in excludes:
-            lock.acquire()
-            excludes.append(key)
-            lock.release()
-            break
-    print(key)
-    return key
+    cell = numpy.random.randint(0,len(layout))
+    collum = random.randint(0,len(layout[cell]))
+    return cell, collum
 
-def findkey(k):
-    for row, value in enumerate(layout):
-        #print(row)
-        for cell, i in enumerate(value):
-            if i == k:
-                return [row,cell]
-
-
-def keyswap(k, pk):
-    print('A')
-    keyboard.press_and_release(pk)
-    schlock.acquire()
-    #keyboard.unremap_key(k)
-    keyboard.remove_hotkey(k)
-    schlock.release()
-    lock.acquire()
-    excludes.remove(k)
-    lock.release()
-    muck(randkey())
-
-def muck(key):
-    v, h = findkey(key)
-    voff = hoff = 0
-    while voff + hoff == 0:
-        voff = random.getrandbits(1)
-        hoff = random.getrandbits(1)
-    voffloc = (v + voff) % len(layout)
-    pressKey = (layout[voffloc][(h + hoff) % len(layout[voffloc])])
-    #keyboard.remap_key(key, (layout[voffloc][(h + hoff) % len(layout[voffloc])]))
-    #keyboard.hook_key(key, keyswap(key))
-    keyboard.add_hotkey(key, keyswap, args=[key, pressKey],suppress=True)
-    return key
-
-def listen():
-    while True:
-        key = muck(randkey())
-        while True:
-            if(keyboard.is_pressed(key)):
-                keyswap(key)
-                break
-        print('AAA')
-
-
-def runMouse(chance):
+def missClickOLD(chance):
     while True:
         if (mouse.is_pressed() and not has_moved):
             has_moved = True
@@ -86,14 +31,23 @@ def runMouse(chance):
                 print('ran')
         if not mouse.is_pressed():
             has_moved = False
-def runMouse2(chance):
-    prev_x, prev_y = mouse.get_position()
-    while True:
-        now_x, now_y = mouse.get_position()
+
+def missClick(chance):
+    if (random.uniform(0, 100) < chance):
+        mouse.move(numpy.random.randint(20) * numpy.random.choice([-1, 1]),
+                   numpy.random.randint(20) * numpy.random.choice([-1, 1]), False,
+                   numpy.random.random_sample() * .05)
+        print('ran')
+
+
+def randomDrag(chance):
+    #prev_x, prev_y = mouse.get_position()
+    #while True:
+        """now_x, now_y = mouse.get_position()
         if abs(now_x-prev_x > 8 and abs(now_y-prev_y) > 5):
             if (numpy.random.randint(chance) == 1):
-                mouse.right_click()
-        prev_x, prev_y = mouse.get_position()
+                mouse.right_click()"""
+        #prev_x, prev_y = mouse.get_position()
 
 
 def runClipboard(cooldown):
@@ -113,21 +67,49 @@ def runClipboard(cooldown):
             demp[a - 1 % len(list(dump))] = b
             pyperclip.copy(''.join(demp))
 
-for key in range(amt):
-    muck(randkey())
-    '''thread = threading.Thread(target=listen)
-    thread.start()
-    '''
-clipboardThread = threading.Thread(target=runClipboard, args=(5*60,))
-#clipboardThread.setDaemon(True)
-clipboardThread.start()
-time.sleep(.25)
-mouseThread = threading.Thread(target=runMouse, args=(10,))
-#mouseThread.setDaemon(True)
-mouseThread.start()
 
-mouseThread2 = threading.Thread(target=runMouse2, args=(50,))
-#mouseThread.setDaemon(True)
-mouseThread2.start()
-while True:
-    pass
+def muck(event):
+    #print("Name: " + event.name)
+    #print(event.name == 'a')
+    cell = collum = -1
+    for x in range(len(layout)):
+        for y in range(len(layout[x])):
+            #print(layout[x][y])
+            if (layout[x][y] == event.name):
+                cell = x
+                collum = y
+    #if(cell or collum == -1):
+     #   exit("no ifnd")
+
+
+    if(random.uniform(0,100) < keyboardOdds):
+        randKey = layout[cell + numpy.random.randint(-1,1)][collum + numpy.random.randint(-1,1)]
+        keyboard.press(randKey)
+        keyboard.release(randKey)
+    else:
+        keyboard.press(event.name)
+
+def main():
+    """for i in range(amt):
+        a,b = randkey()
+        initBind(a,b)
+        print("A " + a)
+        print("B " + b)"""
+    clipboardThread = threading.Thread(target=runClipboard, args=(clipClearCooldown,))
+    # clipboardThread.setDaemon(True)
+    clipboardThread.start()
+    #mouseThread = threading.Thread(target=missClick, args=(missClickChance,))
+    # mouseThread.setDaemon(True)
+    #mouseThread.start()
+
+    #mouseThread2 = threading.Thread(target=randomDrag, args=(randomDragChance,))
+    # mouseThread.setDaemon(True)
+    #mouseThread2.start()
+    mouse.on_click(missClick, [missClickChance])
+    keyboard.on_press(muck, True)
+    keyboard.wait()
+
+
+
+if(__name__ == "__main__"):
+    main()
